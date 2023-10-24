e18c9a2c92c69c89ba894de762f0cd374705e0fc
abhinav3254 <abhinav3254@gmail.com>
2023-10-24 17:30:03 +0530

    fix search

--- src/app/components/nav-bar/NavBarComponent.ts
+++ src/app/components/nav-bar/NavBarComponent.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DatasharingserviceService } from '../home/datasharingservice.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,10 +9,13 @@ import { DatasharingserviceService } from '../home/datasharingservice.service';
     styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-    constructor(private dataSharingService: DatasharingserviceService) { }
+    constructor(private dataSharingService: DatasharingserviceService,
+        private router: Router
+    ) { }
     searchText: string = '';
 
     searchIt(searchText: string) {
         this.dataSharingService.setSearchText(this.searchText);
+        this.router.navigateByUrl('/home');
     }
 }
