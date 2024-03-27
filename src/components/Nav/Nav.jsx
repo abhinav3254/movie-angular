d8732f7f6d99fc03004de0524f166927f53cf14a
abhinav3254 <abhinav3254@gmail.com>
2024-03-27 16:44:34 +0530

    side bar added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
 import './Nav.scss'
 import logo from '../../images/logo.svg'
 import searchIcon from '../../images/search.svg'
 
 function Nav() {
+    const [visibleRight, setVisibleRight] = useState(false);
     return (
         <div className='Nav'>
             <img className='NavLogo' src={logo} alt="" />
@@ -21,6 +24,16 @@ function Nav() {
                 <input className='NavRightSearchInput' type="text" placeholder='search...' />
                 <img className='NavRightSearchIcon' src={searchIcon} alt="" />
             </div>
+
+            <span className="pi pi-user sidebar-icon" onClick={() => setVisibleRight(true)}></span>
+
+            <Sidebar visible={visibleRight} position="right" onHide={() => setVisibleRight(false)}>
+                <h2>Right Sidebar</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </Sidebar>
 
         </div>
     )
