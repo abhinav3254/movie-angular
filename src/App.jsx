8a0d16041823e8346c39c7869683c146e27a1412
abhinav3254 <abhinav3254@gmail.com>
2024-03-31 18:59:20 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Auth />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/detail' element={<MovieDetail />} />
+        <Route path='/detail/:id' element={<MovieDetail />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
